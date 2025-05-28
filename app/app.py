1fefde3eab29fee4d5732d2d6f2708014a43b079
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-05-28 00:19:12 -0400

    Updated templates with Flask formatting

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Personal Finance Tracker is running!</h1>"
+    return render_template('index.html')
+
+@app.route('/add_expense', methods=['GET', 'POST'])
+def add_expense():
+    return render_template('add_expense.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
