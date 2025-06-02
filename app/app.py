c2cd17f45ee7753f17a7c2d5ac6ead89cb4602e8
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-06-02 09:55:24 -0400

    UI completed. Ready to add database logic

--- app/app.py
+++ app/app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/add_expense', methods=['GET', 'POST'])
 def add_expense():
-    return render_template('add_expense.html')
+    if request.method == 'GET':
+        return render_template('add_expense.html')
+    elif request.method == 'POST':
+        return render_template('add_expense.html', expense=request.form['expense'])
 
 @app.route('/reports')
 def reports():
-    # expenses = get_expenses_from_db()  # Your data logic
     return render_template('reports.html')
-    # return render_template('reports.html', expenses=expenses)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
